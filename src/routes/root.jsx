1816aca2a89eb9ff975943471b05f180b8d3cd1b
IbadAsadov <asadov.ibad@gmail.com>
2023-01-19 15:48:17 +0400

    error element adding destroy rout

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -7,7 +7,7 @@ export default function Root() {
 
     return (
         <>
-            <div id="sidebar" className={navigation.state === "loading" ? "loading" : ""}>
+            <div id="sidebar">
                 <h1>React Router Contacts</h1>
                 <div>
                     <form id="search-form" role="search">
@@ -49,7 +49,7 @@ export default function Root() {
                     )}
                 </nav>
             </div>
-            <div id="detail">
+            <div id="detail" className={navigation.state === "loading" ? "loading" : ""}>
                 <Outlet />
             </div>
         </>
