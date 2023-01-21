b32700e531fbec0f1db23a570a11c7c02d335ac9
IbadAsadov <asadov.ibad@gmail.com>
2023-01-21 17:07:03 +0400

    completed

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,20 +1,36 @@
-import { NavLink, Outlet, useLoaderData, Form, redirect, useNavigation } from "react-router-dom";
+import { NavLink, Outlet, useLoaderData, Form, redirect, useNavigation, useSubmit } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export default function Root() {
-    const { contacts } = useLoaderData();
+    const { contacts, q } = useLoaderData();
     const navigation = useNavigation();
+    const submit = useSubmit();
+
+    const searching = navigation.location && new URLSearchParams(navigation.location.search).has("q");
 
     return (
         <>
             <div id="sidebar">
                 <h1>React Router Contacts</h1>
                 <div>
-                    <form id="search-form" role="search">
-                        <input id="q" aria-label="Search contacts" placeholder="Search" type="search" name="q" />
-                        <div id="search-spinner" aria-hidden hidden={true} />
-                        <div className="sr-only" aria-live="polite"></div>
-                    </form>
+                    <Form id="search-form" role="search">
+                        <input
+                            id="q"
+                            className={searching ? "loading" : ""}
+                            aria-label="Search contacts"
+                            placeholder="Search"
+                            type="search"
+                            name="q"
+                            defaultValue={q}
+                            onChange={(event) => {
+                                const isFirstSearch = q == null;
+                                submit(event.currentTarget.form, {
+                                    replace: !isFirstSearch,
+                                });
+                            }}
+                        />
+                        <div id="search-spinner" aria-hidden hidden={!searching} />
+                    </Form>
                     <Form method="post">
                         <button type="submit">New</button>
                     </Form>
@@ -56,9 +72,11 @@ export default function Root() {
     );
 }
 
-export async function loader() {
-    const contacts = await getContacts();
-    return { contacts };
+export async function loader({ request }) {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
+    return { contacts, q };
 }
 
 export async function action() {
